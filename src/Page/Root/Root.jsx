7b821f8f6822145d9e9debe0fce7bdf1d63aacac
Fahmida Nimra <fahmidanimra@gmail.com>
2025-11-18 15:17:45 +0600

    Reduce noise overlay stength to 0.2 and updated the gradient color of whole page at root page

--- src/Page/Root/Root.jsx
+++ src/Page/Root/Root.jsx
@@ -5,7 +5,7 @@ import Navbar from '../Home/Component/Navbar';
 
 const Root = () => {
     return (
-        <div className="relative bg-gradient-to-br from-black via-[#4d016d] to-[#835fe6] min-h-screen overflow-hidden">
+        <div className="relative bg-gradient-to-tr from-black via-[#100919] to-[#9945ff] min-h-screen overflow-hidden">
 
             {/* Grain overlay */}
             <div className="noise-overlay" />
