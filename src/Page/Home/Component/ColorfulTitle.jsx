fcccd61ffb81f61d7fbb765a551321c168be015d
Fahmida Nimra <fahmidanimra@gmail.com>
2025-11-20 23:55:00 +0600

    Added a spping star on colorful title component at top of the page

--- src/Page/Home/Component/ColorfulTitle.jsx
+++ src/Page/Home/Component/ColorfulTitle.jsx
@@ -1,6 +1,8 @@
 
 import React from "react";
-import {ColourfulText} from "../../../components/ui/colourful-text";
+import { ColourfulText } from "../../../components/ui/colourful-text";
+import { Star } from "lucide-react";
+import { PiStarFill } from "react-icons/pi";
 
 
 export default function ColourfulTextDemo() {
@@ -8,8 +10,8 @@ export default function ColourfulTextDemo() {
         <div className="relative">
 
             <h1
-                className="text-[17px]   tracking-wide border-1 w-96 mx-auto font-bold px-5 py-1 rounded-full text-[#d633ff] kaushan  text-center">
-                <ColourfulText text="Painting the Web with  Code and Color" /> 
+                className="text-[16px] flex items-center  bg-black/20 tracking-wide border-1 border-purple-600 w-96 mx-auto font-bold px-5 py-1 rounded-full text-[#d633ff] kaushan  text-center">
+               <PiStarFill className="text-yellow-300 mr-3 animate-spin"></PiStarFill> <ColourfulText text="Painting the Web with  Code and Color" />
             </h1>
         </div>
     );
